a254f6c05a2b903d58af1fc998668692f5580ccb
Elijah Lennon <elijahlennon1@gmail.com>
2022-07-06 18:58:17 -0500

    Completed Exercise

--- WeatherMapExercise/Program.cs
+++ WeatherMapExercise/Program.cs
@@ -9,13 +9,14 @@ namespace WeatherMapExercise
         static void Main(string[] args)
         {
             string key = File.ReadAllText("appsettings.json");
-            string APIkey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
 
             Console.WriteLine("Please enter your zipcode.");
 
             var zipCode = Console.ReadLine();
 
-            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIkey}";
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
 
             Console.WriteLine();
 
